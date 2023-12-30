18c665a219e6772fd6b9c84976084a05440a7732
yujianke100 <yujianke100@gmail.com>
2023-12-31 01:50:29 +1100

    update

--- src/extension.ts
+++ src/extension.ts
@@ -5,15 +5,14 @@ import * as fs from 'fs';
 import * as os from 'os';
 
 export async function activate(context: vscode.ExtensionContext) {
-    console.log('Activating UTS Server extension.'); // 调试信息
+    console.log('Activating UTS Server extension.'); 
 	try {
-        // 等待 SSH 配置检查完成
         await checkAndCompleteUtsConfig();
     } catch (error) {
-        return; // 如果配置不完整，终止激活流程
+        return; 
     }
     const serverProvider = new ServerProvider();
-    vscode.window.registerTreeDataProvider('uts-ihpc-list', serverProvider); // 确保这里的ID与package.json中的views ID相匹配
+    vscode.window.registerTreeDataProvider('uts-ihpc-list', serverProvider);
 	context.subscriptions.push(vscode.commands.registerCommand('uts-ihpc.refresh', () => {
         serverProvider.refresh();
     }));
@@ -29,10 +28,8 @@ async function checkAndCompleteUtsConfig() {
         configContent = await fs.promises.readFile(sshConfigPath, 'utf8');
     } catch (error: any) {
         if (error.code === 'ENOENT') {
-            // 文件不存在
             configContent = '';
         } else {
-            // 读取文件时出现其他错误
             vscode.window.showErrorMessage(`Failed to read SSH configuration: ${error.message}`);
             return;
         }
@@ -40,7 +37,6 @@ async function checkAndCompleteUtsConfig() {
 
     const utsConfigExists = configContent.includes('Host uts');
     if (!utsConfigExists) {
-        // Host uts 不存在，提示用户输入用户名
         const userName = await vscode.window.showInputBox({
             prompt: 'Enter your username for Host uts',
             placeHolder: 'username',
@@ -76,31 +72,23 @@ async function configureSsh(nodeName: string) {
 	}
 }
 function updateSshConfig(config: string, nodeName: string): string {
-    // 分割配置文件为行
     const lines = config.split('\n');
-
-    // 检查是否已经包含指定的节点
     if (lines.some(line => line.startsWith(`Host ${nodeName}`))) {
-        return config; // 如果节点已存在，不需要修改
+        return config; 
     }
 
-    // 提取 "Host uts" 的 User 设置
     const utsUserMatch = config.match(/Host uts\n\s*HostName [^\n]+\n\s*User ([^\n]+)/);
     const utsUser = utsUserMatch ? utsUserMatch[1] : '';
 	if(utsUser === '') {
 		checkAndCompleteUtsConfig();
 	}
 
-    // 构建新的节点配置
     const newNodeConfig = `Host ${nodeName}\n  HostName ${nodeName}\n  User ${utsUser}\n  ProxyJump uts`;
 
-    // 定义节点顺序
     const nodeOrder = ['jupiter', 'mars', 'mercury', 'neptune', 'saturn', 'venus', 'uts'];
 
-    // 提取所有 "Host" 行
     const hostLines = lines.filter(line => line.startsWith('Host '));
 
-    // 排序函数，按照节点顺序和数字排序
     function getNodeOrderIndex(hostLine: string): number {
 		const hostName = hostLine.split(' ')[1];
 		const prefixMatch = hostName.match(/^[a-zA-Z]+/);
@@ -113,7 +101,6 @@ function updateSshConfig(config: string, nodeName: string): string {
 		return numberMatch ? parseInt(numberMatch[0], 10) : 0;
 	}
 
-    // 找到合适的插入位置
     const insertIndex = hostLines.findIndex(hostLine => {
         const orderIndex = getNodeOrderIndex(hostLine);
         const newOrderIndex = getNodeOrderIndex(`Host ${nodeName}`);
@@ -123,7 +110,6 @@ function updateSshConfig(config: string, nodeName: string): string {
         return getNodeNumber(hostLine) > getNodeNumber(`Host ${nodeName}`);
     });
 
-    // 在合适位置插入新配置
     if (insertIndex !== -1) {
         const realInsertIndex = lines.indexOf(hostLines[insertIndex]);
         lines.splice(realInsertIndex, 0, newNodeConfig);
@@ -146,22 +132,21 @@ class ServerProvider implements vscode.TreeDataProvider<ServerNode> {
     }
 
     getChildren(): Thenable<ServerNode[]> {
-        console.log('Fetching server data...'); // 调试信息
+        console.log('Fetching server data...'); 
         return new Promise(resolve => {
             exec('ssh uts "cnode | grep yes"', (error, stdout, stderr) => {
                 if (error) {
-                    console.error('Error fetching data:', stderr); // 调试信息
+                    console.error('Error fetching data:', stderr);
                     vscode.window.showErrorMessage(`Error: ${stderr}`);
                     return resolve([]);
                 }
-                console.log('Data fetched:', stdout); // 调试信息
+                console.log('Data fetched:', stdout); 
                 resolve(this.parseServerOutput(stdout));
             });
         });
     }
 
     parseServerOutput(output: string): ServerNode[] {
-		// 设置各列的最大长度
 		const nameMaxLength = 'mercury28'.length;
 		const cpuMemGpuMaxLength = '100.0%'.length;
 	
