a434fb35fb2649de0604cfe2055d9887dd5043b3
yujianke100 <yujianke100@gmail.com>
2023-12-31 20:35:38 +1100

    update

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ Helping you easily use UTS iHPC on VS Code.
 
 ## Requirements
 
-you need to be in the UTS intranet environment or have an active [VPN](https://vpn.uts.edu.au/) connection established. 
+1. You need to be in the UTS intranet environment or have an active [VPN](https://vpn.uts.edu.au/) connection established. 
+2. You need to ensure that your device can log in to iHPC without password. You can achieve this by setting a [SSH key](https://code.visualstudio.com/docs/remote/troubleshooting#_quick-start-using-ssh-keys).
 
 **Recommended**: Use in Conjunction with [Remote-SSH](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-ssh).
 
@@ -24,6 +25,10 @@ If you do not have an account yet, please apply for one at [this page](https://i
 
 ## Release Notes
 
+### 1.0.1
+
+Add the function of one click connection to SSH
+Add display of username
 
 ### 1.0.0
 
@@ -35,7 +40,7 @@ Publish the extension.
 
 **Node Overview**: Access the extension panel to confirm the currently available nodes and their status within your UTS iHPC cluster. Information includes CPU usage, memory usage, GPU availability, GPU memory usage, and the username of the user currently utilizing the node. 
 
-**One-Click Node Connection**: By clicking on a specific node in the panel, the plugin automatically updates the connection details in the Remote-SSH plugin. Be sure to refresh the list in the Remote-SSH plugin, and the newly added node is readily available for your remote access needs.
+**One-Click Node Connection**: Right-clicking on a specific node can open a new command to log in to the node with ssh or update the connection details in the Remote-SSH. Be sure to refresh the list in the Remote-SSH plugin, and the newly added node is readily available for your remote access needs.
 
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,121 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as os from 'os';
 
+const utsUserStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+utsUserStatusBarItem.command = 'uts-ihpc.openSettings';
+
 export async function activate(context: vscode.ExtensionContext) {
-    console.log('Activating UTS Server extension.'); 
-	try {
+    console.log('Activating UTS Server extension.');
+    try {
         await checkAndCompleteUtsConfig();
     } catch (error) {
-        return; 
+        return;
     }
     const serverProvider = new ServerProvider();
     vscode.window.registerTreeDataProvider('uts-ihpc-list', serverProvider);
-	context.subscriptions.push(vscode.commands.registerCommand('uts-ihpc.refresh', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('uts-ihpc.refresh', () => {
         serverProvider.refresh();
     }));
-    context.subscriptions.push(vscode.commands.registerCommand('uts-ihpc.configureSsh', async (nodeName) => {
-        await configureSsh(nodeName);
+    context.subscriptions.push(vscode.commands.registerCommand('uts-ihpc.configureSsh', async (node: ServerNode) => {
+        if (node && node.name) {
+            console.log('Configuring SSH for', node.name.trim());
+            await configureSsh(node.name.trim());
+        } else {
+            console.log('No valid node provided for SSH configuration');
+        }
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('uts-ihpc.openSettings', openSettings));
+
+    
+    context.subscriptions.push(utsUserStatusBarItem);
+
+    const treeView = vscode.window.createTreeView('uts-ihpc-list', { treeDataProvider: new ServerProvider() });
+
+    treeView.onDidChangeVisibility(e => {
+        if (e.visible) {
+            updateStatusBarItem();
+        } else {
+            utsUserStatusBarItem.hide();
+        }
+    });
+
+    context.subscriptions.push(vscode.commands.registerCommand('uts-ihpc.connectSsh', async (node: ServerNode) => {
+        if (node && node.name) {
+            await configureSsh(node.name.trim());
+
+            const terminal = vscode.window.createTerminal(`SSH: ${node.name.trim()}`);
+            terminal.show();
+
+            terminal.sendText(`ssh ${node.name.trim()}`);
+        }
     }));
 }
+
+async function updateStatusBarItem() {
+    const sshConfigPath = path.join(os.homedir(), '.ssh', 'config');
+    try {
+        const configContent = await fs.promises.readFile(sshConfigPath, 'utf8');
+        const utsUserMatch = configContent.match(/Host uts\n\s*HostName [^\n]+\n\s*User (\S+)/);
+        const utsUser = utsUserMatch ? utsUserMatch[1] : 'Not Set';
+
+        utsUserStatusBarItem.text = `UTS User: ${utsUser}`;
+        utsUserStatusBarItem.show();
+    } catch (error) {
+        utsUserStatusBarItem.text = `UTS User: Not Available`;
+        utsUserStatusBarItem.show();
+    }
+}
+
+async function openSettings() {
+
+    updateUtsUsernameInSshConfig();
+}
+
+async function updateUtsUsernameInSshConfig() {
+    const sshConfigPath = path.join(os.homedir(), '.ssh', 'config');
+
+    let configContent;
+    try {
+        configContent = await fs.promises.readFile(sshConfigPath, 'utf8');
+    } catch (error: any) {
+        if (error.code === 'ENOENT') {
+            configContent = '';
+        } else {
+            vscode.window.showErrorMessage(`Failed to read SSH configuration: ${error.message}`);
+            return;
+        }
+    }
+
+    const userName = await vscode.window.showInputBox({
+        prompt: 'Enter your username for Host uts',
+        placeHolder: 'username',
+        ignoreFocusOut: true
+    });
+
+    if (userName) {
+        const utsConfigRegex = /Host uts\s*\n\s*HostName access\.ihpc\.uts\.edu\.au\s*\n\s*User \S+/;
+        const utsConfigStr = `Host uts\n  HostName access.ihpc.uts.edu.au\n  User ${userName}\n`;
+
+        if (utsConfigRegex.test(configContent)) {
+            configContent = configContent.replace(utsConfigRegex, utsConfigStr);
+        } else {
+            configContent = utsConfigStr + '\n' + configContent;
+        }
+
+        try {
+            await fs.promises.writeFile(sshConfigPath, configContent);
+            vscode.window.showInformationMessage('SSH configuration for UTS iHPC updated successfully.');
+            updateStatusBarItem();
+            //refresh tree view
+            vscode.commands.executeCommand('uts-ihpc.refresh');
+        } catch (writeError: any) {
+            vscode.window.showErrorMessage(`Failed to write SSH configuration: ${writeError.message}`);
+        }
+    } else {
+        vscode.window.showErrorMessage('SSH configuration requires a username for UTS iHPC.');
+    }
+}
+
 async function checkAndCompleteUtsConfig() {
     const sshConfigPath = path.join(os.homedir(), '.ssh', 'config');
 
@@ -58,8 +157,8 @@ async function checkAndCompleteUtsConfig() {
 }
 
 
-
 async function configureSsh(nodeName: string) {
+    console.log('Configuring SSH for', nodeName)
     const sshConfigPath = path.join(os.homedir(), '.ssh', 'config');
     try {
         const configContent = await fs.promises.readFile(sshConfigPath, 'utf8');
@@ -67,39 +166,55 @@ async function configureSsh(nodeName: string) {
         await fs.promises.writeFile(sshConfigPath, updatedContent);
         vscode.window.showInformationMessage(`SSH configuration updated for ${nodeName}`);
     } catch (error: any) {
-		console.error(error);
-		vscode.window.showErrorMessage(`Failed to update SSH configuration: ${error.message}`);
-	}
+        console.error(error);
+        vscode.window.showErrorMessage(`Failed to update SSH configuration: ${error.message}`);
+    }
 }
 function updateSshConfig(config: string, nodeName: string): string {
     const lines = config.split('\n');
-    if (lines.some(line => line.startsWith(`Host ${nodeName}`))) {
-        return config; 
-    }
 
     const utsUserMatch = config.match(/Host uts\n\s*HostName [^\n]+\n\s*User ([^\n]+)/);
     const utsUser = utsUserMatch ? utsUserMatch[1] : '';
-	if(utsUser === '') {
-		checkAndCompleteUtsConfig();
-	}
+    if (utsUser === '') {
+        checkAndCompleteUtsConfig();
+    }
 
     const newNodeConfig = `Host ${nodeName}\n  HostName ${nodeName}\n  User ${utsUser}\n  ProxyJump uts`;
+
+    const hostIndex = lines.findIndex(line => line.startsWith(`Host ${nodeName}`));
+    if (hostIndex !== -1) {
+        let userLineUpdated = false;
+        for (let i = hostIndex + 1; i < lines.length && !lines[i].startsWith('Host '); i++) {
+            if (lines[i].startsWith('  User ')) {
+                lines[i] = `  User ${utsUser}`;
+                userLineUpdated = true;
+                break;
+            }
+        }
+
+        if (!userLineUpdated) {
+            let nextHostIndex = lines.findIndex((line, index) => index > hostIndex && line.startsWith('Host '));
+            lines.splice(hostIndex, nextHostIndex - hostIndex, newNodeConfig);
+        }
+
+        return lines.join('\n');
+    }
 
     const nodeOrder = ['jupiter', 'mars', 'mercury', 'neptune', 'saturn', 'venus', 'uts'];
 
     const hostLines = lines.filter(line => line.startsWith('Host '));
 
     function getNodeOrderIndex(hostLine: string): number {
-		const hostName = hostLine.split(' ')[1];
-		const prefixMatch = hostName.match(/^[a-zA-Z]+/);
-		const prefix = prefixMatch ? prefixMatch[0] : '';
-		const index = nodeOrder.indexOf(prefix);
-		return index !== -1 ? index : nodeOrder.length;
-	}
+        const hostName = hostLine.split(' ')[1];
+        const prefixMatch = hostName.match(/^[a-zA-Z]+/);
+        const prefix = prefixMatch ? prefixMatch[0] : '';
+        const index = nodeOrder.indexOf(prefix);
+        return index !== -1 ? index : nodeOrder.length;
+    }
     function getNodeNumber(hostLine: string): number {
-		const numberMatch = hostLine.match(/[0-9]+/);
-		return numberMatch ? parseInt(numberMatch[0], 10) : 0;
-	}
+        const numberMatch = hostLine.match(/[0-9]+/);
+        return numberMatch ? parseInt(numberMatch[0], 10) : 0;
+    }
 
     const insertIndex = hostLines.findIndex(hostLine => {
         const orderIndex = getNodeOrderIndex(hostLine);
@@ -107,7 +222,14 @@ function updateSshConfig(config: string, nodeName: string): string {
         if (orderIndex !== newOrderIndex) {
             return orderIndex > newOrderIndex;
         }
-        return getNodeNumber(hostLine) > getNodeNumber(`Host ${nodeName}`);
+    
+        const existingNodeNumber = getNodeNumber(hostLine);
+        const newNodeNumber = getNodeNumber(`Host ${nodeName}`);
+        if (existingNodeNumber !== newNodeNumber) {
+            return existingNodeNumber > newNodeNumber;
+        }
+    
+        return hostLine > `Host ${nodeName}`;
     });
 
     if (insertIndex !== -1) {
@@ -132,51 +254,64 @@ class ServerProvider implements vscode.TreeDataProvider<ServerNode> {
     }
 
     getChildren(): Thenable<ServerNode[]> {
-        console.log('Fetching server data...'); 
-        return new Promise(resolve => {
-            exec('ssh uts "cnode | grep yes"', (error, stdout, stderr) => {
+        return new Promise((resolve, reject) => {
+            const command = 'ssh uts "cnode | grep yes"';
+            const timeout = 10000;
+
+            const child = exec(command, { timeout: timeout }, (error, stdout, stderr) => {
                 if (error) {
                     console.error('Error fetching data:', stderr);
                     vscode.window.showErrorMessage(`Error: ${stderr}`);
                     return resolve([]);
                 }
-                console.log('Data fetched:', stdout); 
+                console.log('Data fetched:', stdout);
                 resolve(this.parseServerOutput(stdout));
             });
+
+
+            setTimeout(() => {
+                if (child.exitCode === null) {
+                    child.kill();
+                    vscode.window.showErrorMessage("SSH command timed out. Please check your network connection, username and SSH Key.");
+                }
+            }, timeout);
         });
     }
 
     parseServerOutput(output: string): ServerNode[] {
-		const nameMaxLength = 'mercury28'.length;
-		const cpuMemGpuMaxLength = '100.0%'.length;
-	
-		const lines = output.split('\n');
-		return lines.map(line => {
-			if (line.startsWith('*') || line.trim() === '') return null;
-			const parts = line.trim().split(/\s+/);
-			if (parts.length < 6) return null;
-	
-			const name = parts[0].padEnd(nameMaxLength, ' ');
-			const cpu = parts[3].padEnd(cpuMemGpuMaxLength, ' ');
-			const mem = parts[4].padEnd(cpuMemGpuMaxLength, ' ');
-			const gpu = parts.length > 6 ? parts[5].padEnd(cpuMemGpuMaxLength, ' ') : 'N/A'.padEnd(cpuMemGpuMaxLength, ' ');
-			const gpuMem = parts.length > 6 ? parts[6].padEnd(cpuMemGpuMaxLength, ' ') : 'N/A'.padEnd(cpuMemGpuMaxLength, ' ');
-			const users = parts.length > 7 ? parts.slice(7).join(', ') : '';
-	
-			return new ServerNode(`${name}`,`| C: ${cpu} | M: ${mem} | G: ${gpu} | GM: ${gpuMem} | ${users}`);
-		}).filter(node => node !== null) as ServerNode[];
-	}
-	
+        const nameMaxLength = 'mercury28'.length;
+        const cpuMemGpuMaxLength = '100.0%'.length;
+
+        const lines = output.split('\n');
+        return lines.map(line => {
+            if (line.startsWith('*') || line.trim() === '') return null;
+            const parts = line.trim().split(/\s+/);
+            if (parts.length < 6) return null;
+
+            const name = parts[0].padEnd(nameMaxLength, ' ');
+            const cpu = parts[3].padEnd(cpuMemGpuMaxLength, ' ');
+            const mem = parts[4].padEnd(cpuMemGpuMaxLength, ' ');
+            const gpu = parts.length > 6 ? parts[5].padEnd(cpuMemGpuMaxLength, ' ') : 'N/A'.padEnd(cpuMemGpuMaxLength, ' ');
+            const gpuMem = parts.length > 6 ? parts[6].padEnd(cpuMemGpuMaxLength, ' ') : 'N/A'.padEnd(cpuMemGpuMaxLength, ' ');
+            const users = parts.length > 7 ? parts.slice(7).join(', ') : '';
+
+            return new ServerNode(`${name}`, `| C: ${cpu} | M: ${mem} | G: ${gpu} | GM: ${gpuMem} | ${users}`);
+        }).filter(node => node !== null) as ServerNode[];
+    }
+
+
 }
 
 class ServerNode extends vscode.TreeItem {
     constructor(public readonly name: string, public readonly description: string) {
         super(name, vscode.TreeItemCollapsibleState.None);
+        this.contextValue = 'ServerNode';
         this.tooltip = `${this.name} - ${this.description}`;
         this.command = {
             title: "Configure SSH",
             command: "uts-ihpc.configureSsh",
-            arguments: [this.name]
+            arguments: [this.name.trim()]
         };
+
     }
 }
